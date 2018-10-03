ee5c021434829f02dc6b655b70ec2cfcaa5e6feb
smart2538 <worapon.o@ku.th>
2018-10-03 23:56:09 +0700

    add image and wording

---
+++ pun4tress/src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="hello backgroundImage">
+    <div class="content">
+      <h1>{{ title }}</h1>
+      <h2>{{ description }}</h2>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      title: 'Pun4Tress',
+      description: '18 BIRTHDAY PROJECT'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+  color: whitesmoke;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.backgroundImage{
+  background-image: url("../assets/images/background.jpg");
+      /* Full height */
+    height: 100%; 
+
+    /* Center and scale the image nicely */
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+.content{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    height: 100%;
+}
+</style>
